10509e5ab43f22ada7ad448d602678e74e8af330
Robert <robertgibizov@gmail.com>
2025-02-18 19:47:37 +0300

    models

--- www/database/migrations/2025_02_18_155931_create_posts_table.php
+++ www/database/migrations/2025_02_18_155931_create_posts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
